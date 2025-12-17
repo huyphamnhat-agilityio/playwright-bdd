9032e79b2bcd75d7c99999e5261bafd0ffb7bc0f
Huy Nhat Pham <huy.phamnhat@asnet.com.vn>
2025-12-17 15:04:08 +0700

    fix: fix fail login test case

--- src/steps/auth/auth.steps.ts
+++ src/steps/auth/auth.steps.ts
@@ -28,7 +28,7 @@ When('the user clicks the login button', async ({ loginPage }) => {
 });
 
 Then('the user should navigate to the dashboard page', async ({ page }) => {
-  const dashboardLogo = page.getByRole('alert', {
+  const dashboardLogo = page.getByRole('link', {
     name: UI_ELEMENTS.POCKETBASE_LOGO,
   });
   await expect(dashboardLogo).toBeVisible();
